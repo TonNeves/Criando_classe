52bf8f1711176054cfff196b862d19fd82915444
TonNeves <esterdiem@gmail.com>
2021-03-03 00:52:50 -0300

    adicionando private set na classe Pessoa

--- src/one/digitalinnovation/digitalonebranck/Pessoa.kt
+++ src/one/digitalinnovation/digitalonebranck/Pessoa.kt
@@ -1,15 +1,18 @@
 package one.digitalinnovation.digitalonebranck
 
 class Pessoa {
-    val nome: String = "Washington"
-    val cpf: String = "770.490.802-25"
+    var nome: String = "Washington"
+    var cpf: String = "770.490.802-25"
+    private set
 
     inner class Endereco {
         var rua: String = "Alameda Orquideas"
+
     }
 }
 fun main(){
     val Tom = Pessoa()
+
 
     println (Tom.nome)
     println (Tom.cpf)
